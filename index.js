4bb02495066560609b223cb55a2b95f3109aae14
azizmarrouki <75527245+azizmarrouki@users.noreply.github.com>
2021-01-30 11:08:54 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+const rootElement = document.getElementById("root");//We call this a “root” DOM node because everything inside it will be managed by React DOM.
+ReactDOM.render(<App />,rootElement);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
